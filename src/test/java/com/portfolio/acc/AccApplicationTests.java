3e487f22d56ad84a805a65a3f6961ae77117f5c8
AalaNC <AalaN1611@gmail.com>
2022-11-01 00:20:58 -0300

    Solucion error

--- src/test/java/com/portfolio/acc/AccApplicationTests.java
+++ src/test/java/com/portfolio/acc/AccApplicationTests.java
@@ -1,10 +1,10 @@
-package com.portfolio.mgb;
+package com.portfolio.acc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MgbApplicationTests {
+class AccApplicationTests {
 
 	@Test
 	void contextLoads() {
