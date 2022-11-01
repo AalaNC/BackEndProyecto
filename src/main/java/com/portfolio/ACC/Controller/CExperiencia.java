966fb092848a26d26521c60bc0725bc0d29dbbd7
AalaNC <AalaN1611@gmail.com>
2022-11-01 18:32:21 -0300

    CorrigiendoOrigins

--- src/main/java/com/portfolio/ACC/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/ACC/Controller/CExperiencia.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/explab")
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://accfrontend.web.app")
+@CrossOrigin(origins = "https://ap-carrizoalan.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
